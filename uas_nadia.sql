0f34384361ad5844141212f477f7f4b65549d0e1
Nadia321-html <1223150109@global.ac.id>
2025-06-18 02:03:38 +0700

    database

--- uas_nadia.sql
+++ uas_nadia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 17, 2025 at 07:08 PM
+-- Generation Time: Jun 17, 2025 at 09:03 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 5.6.40
 
@@ -21,6 +21,51 @@ SET time_zone = "+00:00";
 --
 -- Database: `uas_nadia`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `barang_id` int(11) NOT NULL,
+  `kategori_id` int(11) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `harga` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`barang_id`, `kategori_id`, `nama_barang`, `harga`) VALUES
+(1, 1, 'Mie Rendang', '4000'),
+(2, 1, 'Mie Aceh', '4000'),
+(3, 3, 'Iphone 11 Promax', '13000000'),
+(4, 4, 'Nike Air Pro', '3000000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori_barang`
+--
+
+CREATE TABLE `kategori_barang` (
+  `kategori_id` int(11) NOT NULL,
+  `nama_kategori` varchar(50) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kategori_barang`
+--
+
+INSERT INTO `kategori_barang` (`kategori_id`, `nama_kategori`) VALUES
+(1, 'Mie'),
+(2, 'minyak'),
+(3, 'handphone'),
+(4, 'sepatu'),
+(5, 'campuran');
 
 -- --------------------------------------------------------
 
@@ -44,11 +89,77 @@ INSERT INTO `operator` (`operator_id`, `nama_lengkap`, `username`, `password`, `
 (1, 'admin', 'admin', '$2y$10$cyiLPz1zUxWmtV8.72Hu5OE6RDGq0GXmrtVewJIeR9pWiv6d', '0000-00-00'),
 (2, 'operator 2', 'operator 2', '3f3c15defbb2909b0eceadd20d3bea93', '2025-06-17'),
 (6, 'operator 3', 'operator 3', '3f3c15defbb2909b0eceadd20d3bea93', '2025-06-17'),
-(9, 'operator', 'operator', '3f3c15defbb2909b0eceadd20d3bea93', '2025-06-17');
+(9, 'operator', 'operator', '3f3c15defbb2909b0eceadd20d3bea93', '2025-06-17'),
+(10, 'operator', 'operator4', '3f3c15defbb2909b0eceadd20d3bea93', '2025-06-17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `transaksi_id` int(11) NOT NULL,
+  `tanggal_transaksi` date NOT NULL,
+  `operator_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`transaksi_id`, `tanggal_transaksi`, `operator_id`) VALUES
+(1, '2023-12-19', 2),
+(2, '2024-01-02', 2),
+(3, '2024-01-02', 2),
+(4, '2024-01-02', 2),
+(5, '2024-01-06', 1),
+(6, '2024-01-06', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi_detail`
+--
+
+CREATE TABLE `transaksi_detail` (
+  `t_detail_id` int(11) NOT NULL,
+  `barang_id` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `transaksi_id` int(11) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `status` enum('0','1') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi_detail`
+--
+
+INSERT INTO `transaksi_detail` (`t_detail_id`, `barang_id`, `qty`, `transaksi_id`, `harga`, `status`) VALUES
+(1, 4, 1, 1, 3000000, '1'),
+(2, 3, 1, 2, 13000000, '1'),
+(3, 3, 1, 3, 13000000, '1'),
+(4, 3, 1, 4, 13000000, '1'),
+(6, 2, 10, 5, 4000, '1'),
+(7, 1, 10, 5, 4000, '1'),
+(8, 1, 5, 6, 4000, '1'),
+(9, 4, 1, 6, 3000000, '1');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`barang_id`);
+
+--
+-- Indexes for table `kategori_barang`
+--
+ALTER TABLE `kategori_barang`
+  ADD PRIMARY KEY (`kategori_id`);
 
 --
 -- Indexes for table `operator`
@@ -57,14 +168,50 @@ ALTER TABLE `operator`
   ADD PRIMARY KEY (`operator_id`);
 
 --
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`transaksi_id`);
+
+--
+-- Indexes for table `transaksi_detail`
+--
+ALTER TABLE `transaksi_detail`
+  ADD PRIMARY KEY (`t_detail_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `barang_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `kategori_barang`
+--
+ALTER TABLE `kategori_barang`
+  MODIFY `kategori_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `operator`
 --
 ALTER TABLE `operator`
-  MODIFY `operator_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `operator_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `transaksi_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `transaksi_detail`
+--
+ALTER TABLE `transaksi_detail`
+  MODIFY `t_detail_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
